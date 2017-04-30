3277a17ebd1537847d740fb33d888d5d933f9b93
jsirianni <jsirianni@ws1.sir.local>
2017-04-30 12:51:38 -0400

    fixed server crashes on port scan

--- In_Class_3/WebServer.go
+++ In_Class_3/WebServer.go
@@ -5,7 +5,7 @@ import "bufio"
 import "io"
 /*
 Simple server application that listens for a connection
-on all network interfaces and port 7070
+on all network interfaces and port 8080
 */
 
 
@@ -33,22 +33,28 @@ func main() {
 
 
 /*
-Function called to handle each invidivual client
-Reads an incoming HTTP request until a blank line is read
-Prints the request to the terminal
-Closes the connection once done
+Closes the client connection when routine finishes
+Creates a new reader for each incoming connection
+Iterates through client request line by line
+Handles bad request by printing the eorr, does not panic
+Prints all header lines to terminal
+Responds to the client request
 */
 func handleClient(c net.Conn) {
   defer c.Close()
-
   r := bufio.NewReader(c)
 
   // Read each header line one by one, and print to terminal
   for {
+    // Read a single header line
     req, err := r.ReadString('\n')
+
+    // Handle errors, keep service running!
     if err != nil && err != io.EOF {
-      panic(err)
+      checkError(err)
     }
+
+    // If no errors, print the header to the terminal
     fmt.Print(req)
 
     // Break loop after reading all headers
@@ -64,9 +70,10 @@ func handleClient(c net.Conn) {
 
 /*
 Function to handle errors
+Calling this function allows us to handle the error without panicing
 */
 func checkError(err error) {
   if err != nil {
-    fmt.Printf("ERROR: " + err.Error())
+    fmt.Printf("ERROR: " + err.Error() + "\n")
   }
 }
