12ebe0126f72a96b0b8bf5fd697413cfe566ade7
jsirianni <jsirianni@ws1.sir.local>
2017-04-29 21:33:14 -0400

    Multithread GO server finished

--- In_Class_3/WebServer.go
+++ In_Class_3/WebServer.go
@@ -11,7 +11,7 @@ on all network interfaces and port 7070
 
 
 // Standard TCP response, will likely be removed later
-const standardResponse = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 70\nConnection: close\n"
+const standardResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 70\r\nConnection: close\r\n"
 
 
 
@@ -40,30 +40,31 @@ Closes the connection once done
 */
 func handleClient(c net.Conn) {
   defer c.Close()
-    r := bufio.NewReader(c)
-    for {
-      req, err := r.ReadString('\n')
-      if err != nil && err != io.EOF {
-        panic(err)
-      }
-      fmt.Print(req)
 
-      if len(req) <= 2 {
-        fmt.Println(len(req))
-        break
-      }
+  r := bufio.NewReader(c)
 
+  // Read each header line one by one, and print to terminal
+  for {
+    req, err := r.ReadString('\n')
+    if err != nil && err != io.EOF {
+      panic(err)
     }
+    fmt.Print(req)
 
-
+    // Break loop after reading all headers
+    if len(req) <= 2 {
+      break
+    }
+  }
 
   // Send reponse to client
   fmt.Fprintf(c, standardResponse)
 }
 
 
-
-// Function to handle errors
+/*
+Function to handle errors
+*/
 func checkError(err error) {
   if err != nil {
     fmt.Printf("ERROR: " + err.Error())
