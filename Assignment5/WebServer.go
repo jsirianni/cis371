4eebca7753ba7f9f74b7a73c6dfdff6df2fe969c
jsirianni <jsirianni@ws1.sir.local>
2017-05-01 18:00:03 -0400

    code clean up

--- Assignment5/WebServer.go
+++ Assignment5/WebServer.go
@@ -5,21 +5,13 @@ import (
     "fmt"
     "bufio"
     "io"
-    //"io/ioutil"
-    //"os"
-    //"runtime"
+    "io/ioutil"
+    "os"
 )
 /*
-Simple server application that listens for a connection
-on all network interfaces and port 8080
-*/
-
-
-
-/*
-Main function creates a TCP socket on port 7070
-Handles multiple client connections at one time with a go routine
-Continues running after client disconnection
+Main function creates a TCP socket on port 7070 and then waits for a client to
+connect. Each connection is handed to a seprate thread by calling a go routine.
+This allows the main to accept many connections at once.
 */
 func main() {
   fmt.Println("Starting server...")
@@ -32,14 +24,15 @@ func main() {
 }
 
 
-
 /*
-Closes the client connection when routine finishes
-Creates a new reader for each incoming connection
-Iterates through client request line by line
-Handles bad request by printing the eorr, does not panic
-Prints all header lines to terminal
-Responds to the client request
+This function is called as a GO routine from main, allowing multiple connections
+to be established and handled at one time. Each connection is passed to a
+bufio reader. The client request is then read line by line (and printed to the
+terminal) until the line is less than 2 charaters.The loop then breaks.
+
+The response begins with a response header being crafted along with a file being
+selected. The response + body (file) is then pinted to the terminal as well as
+being sent to the client. The connection is then closed.
 */
 func handleClient(c net.Conn) {
   defer c.Close()
@@ -67,23 +60,21 @@ func handleClient(c net.Conn) {
   // Send HTML reponse to client
   var headers = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 70\r\nConnection: keep-alive\r\n\r\n"
 
-  //pwd, _ := os.Getwd()
+  pwd, _ := os.Getwd()
 
-  //body, err := ioutil.ReadFile(pwd + "/test.txt")
-  //if err != nil {
-  //  checkError(err)
-  //}
+  body, err := ioutil.ReadFile(pwd + "/test.txt")
+  if err != nil {
+    checkError(err)
+  }
 
-  fmt.Println(headers + "This is a test\nThis is a test\nThis is a test\nThis is a test\nThis is a test\n")
-  fmt.Fprintf(c, headers + "This is a test\nThis is a test\nThis is a test\nThis is a test\nThis is a test\n" )
+  fmt.Println(headers + string(body))
+  fmt.Fprintf(c, headers + string(body))
 } //Emd handleClient()
 
 
-
-
 /*
-Function to handle errors
-Calling this function allows us to handle the error without panicing
+This function is called when an error is detected. Pass an error as a parameter.
+The error is printed to the terminal without halting the server. 
 */
 func checkError(err error) {
   if err != nil {
