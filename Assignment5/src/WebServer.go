f169d036dd4f06e8cc70105cd9aaddcf6b24206f
jsirianni <jsirianni@ws1.sir.local>
2017-05-03 19:16:08 -0400

    code cleanup

--- Assignment5/src/WebServer.go
+++ Assignment5/src/WebServer.go
@@ -43,58 +43,49 @@ func handleClient(c net.Conn) {
   // Close the conection when function finishes
   defer c.Close()
 
-  // Get current path
-  path, _ := os.Getwd()
+  // Variables to use later
+  path, _ := os.Getwd()  // Current path
+  var request = ""       // Client Request
+  var reqCont = ""       // Requested content type
+  var h = ""             // String to hold the response headers
+
 
   // Create a new readr and create request variable
   r := bufio.NewReader(c)
-  var request = ""
 
 
   // Read each header line one by one, adding to the request if valid
   for {
-    // Read a single header line, check for errors
     line, err := r.ReadString('\n')
     if err != nil && err != io.EOF {
       checkError(err)
     }
-
-    // Add header line to request if it is not a newline (end of request)
+    // Add each line to request until EOF
     if len(line) > 2 {
       request += line
     } else {
         break
     }
-  } // End For Loop
-
-
-  // Print Client request to the terminal before validating it (Debug)
+  }
+  // Print Client request to the terminal (Debug)
   fmt.Println(request)
 
 
   // Check if GET request
   if strings.Contains(request, "GET /") {
     // Create string array. The requested file will be = index[1]
+    // Assign requested content type
     s := strings.Fields(request)
-
-    // Create a string to find the required file
     if s[1] == "/" {
       s[1] = "index.html"
     }
     path += ("/" + strings.ToLower(strings.Trim((s[1]), "/")))
-
-    // Notify the terminal of the requested file (Debug)
-    fmt.Println("Requested file: " + path + "\n")
-
+    reqCont = path
   } else {
-    // If not a get request, close the connection and print to console (Debug)
+    // If not a GET request,
     fmt.Println("Bad request, killing connection" + "\n")
     return
   }
-
-
-  // If valid GET request, create response headers
-  var h = ""
 
 
   // Check if path is valid,
@@ -102,32 +93,33 @@ func handleClient(c net.Conn) {
   if _, err := ioutil.ReadFile(path); os.IsNotExist(err) {
     // Print error to the console
     checkError(err)
-
     // Return a 404 headers + body
     h += "HTTP/1.1 404 Not Found\r\n"
     h += "Content-Type: text/plain\r\n"
-    h += "Content-Length: 200\r\n"
+    h += "Content-Length: 30\r\n"
     h += "Connection: close\r\n\r\n"
     h += "This page does not exist\r\n"
     badHeaders := []byte(h)
     c.Write(badHeaders)
 
 
-  // If the file exist, respond
+  // If the file exist
   } else {
-
     // Read the requested file, determine the length
     responseBody, _ := ioutil.ReadFile(path)
     respLen := strconv.Itoa(len(responseBody))
+
 
     // Begin building response header
     h += "HTTP/1.1 200 OK\r\n"
 
     // Determine content type
-    if strings.Contains(request, ".css") {
+    if strings.Contains(reqCont, "css") {
       h += "Content-Type: text/css\r\n"
-    } else if strings.Contains(request, ".html") {
+    } else if strings.Contains(reqCont, "html") {
       h += "Content-Type: text/html\r\n"
+    } else if strings.Contains(reqCont, ".jpg") {
+      h += "Content-Type: text/plain\r\n"
     } else {
       h += "Content-Type: text/plain\r\n"
     }
@@ -137,6 +129,7 @@ func handleClient(c net.Conn) {
 
     // Force connection close
     h += "Connection: close\r\n\r\n"
+
 
     // Assign response headers to a byte array
     goodHeaders := []byte(h)
@@ -148,13 +141,11 @@ func handleClient(c net.Conn) {
     c.Write(goodHeaders)
     c.Write(responseBody)
   }
-
-} // End handle client
+} // End GO routine
 
 
 /*
-This function is called when an error is detected. Pass an error as a parameter.
-The error is printed to the terminal without halting the server.
+Print errors to the terminal, continue running program / do not crash
 */
 func checkError(err error) {
   if err != nil {
