37d36d7582f6065e1fa5106a7ae51709c2e1769c
jsirianni <jsirianni@ws1.sir.local>
2017-05-09 10:30:13 -0400

    add In_Class_6

---
+++ In_Class_6/friendsform.php
@@ -0,0 +1,17 @@
+<?php
+// Joseph Sirianni
+// In Class 6 - Friends Form
+
+
+$firstName = $_GET['fname'];
+$lastName = $_GET['lname'];
+$pNumber = $_GET['pnumber'];
+$age = $_GET['age'];
+
+echo "This is stupid"
+echo $firstName;
+echo $lastName;
+echo $pNumber;
+echo $age;
+
+?>
