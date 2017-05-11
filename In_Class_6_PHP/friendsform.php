e9ef125cf91ac8f142fd6f114aa540777fc7728a
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-05-11 10:44:45 -0400

    Better formatting

--- In_Class_6_PHP/friendsform.php
+++ In_Class_6_PHP/friendsform.php
@@ -2,18 +2,18 @@
 // Joseph Sirianni
 // In Class 6 - Friends Form
 
-
+// Assign posted variables
 $firstName = $_POST['fname'];
 $lastName = $_POST['lname'];
 $pNumber = $_POST['pnumber'];
 $age = $_POST['age'];
-
-
 ?>
 
+<!--Build the html content -->
 <html>
 <body>
   Thank you for your submission! <br><br>
+  
   First name: <?php echo $firstName; ?><br>
   Last name: <?php echo $lastName; ?><br>
   Phone number: <?php echo $pNumber; ?><br>
