eb9758bdc6e92e96c66fd5fc6f148c8357a84a74
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-05-11 08:24:30 -0400

    Fix comments

--- In_Class_3_GO/src/WebServer.go
+++ In_Class_3_GO/src/WebServer.go
@@ -1,36 +1,32 @@
 package main
+
 import "net"
 import "fmt"
 import "bufio"
 import "io"
+
 /*
 Simple server application that listens for a connection
 on all network interfaces and port 8080
 */
 
-
-
 // Standard TCP response, will likely be removed later
 const standardResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 70\r\nConnection: close\r\n\r\nThis is not the real content because this server is not yet complete."
 
-
-
 /*
-Main function creates a TCP socket on port 7070
-Handles multiple client connections at one time with a go routine
+Main function creates a TCP socket on port 8080
+Handles multiple client connections at one time with a GO routine
 Continues running after client disconnection
 */
 func main() {
-  fmt.Println("Starting server...")
-  socket, _ := net.Listen("tcp", ":8080")
+	fmt.Println("Starting server...")
+	socket, _ := net.Listen("tcp", ":8080")
 
-  for {
-    connection, _ := socket.Accept()
-    go handleClient(connection)
-  }
+	for {
+		connection, _ := socket.Accept()
+		go handleClient(connection)
+	}
 }
-
-
 
 /*
 Closes the client connection when routine finishes
@@ -41,39 +37,38 @@ Prints all header lines to terminal
 Responds to the client request
 */
 func handleClient(c net.Conn) {
-  defer c.Close()
-  r := bufio.NewReader(c)
+	defer c.Close()
+	r := bufio.NewReader(c)
 
-  // Read each header line one by one, and print to terminal
-  for {
-    // Read a single header line
-    req, err := r.ReadString('\n')
+	// Read each header line one by one, and print to terminal
+	for {
+		// Read a single header line
+		req, err := r.ReadString('\n')
 
-    // Handle errors, keep service running!
-    if err != nil && err != io.EOF {
-      checkError(err)
-    }
+		// Handle errors, keep service running!
+		if err != nil && err != io.EOF {
+			checkError(err)
+		}
 
-    // If no errors, print the header to the terminal
-    fmt.Print(req)
+		// If no errors, print the header to the terminal
+		fmt.Print(req)
 
-    // Break loop after reading all headers
-    if len(req) <= 2 {
-      break
-    }
-  }
+		// Break loop after reading all headers
+		if len(req) <= 2 {
+			break
+		}
+	}
 
-  // Send reponse to client
-  fmt.Fprintf(c, standardResponse)
+	// Send reponse to client
+	fmt.Fprintf(c, standardResponse)
 }
-
 
 /*
 Function to handle errors
 Calling this function allows us to handle the error without panicing
 */
 func checkError(err error) {
-  if err != nil {
-    fmt.Printf("ERROR: " + err.Error() + "\n\n")
-  }
+	if err != nil {
+		fmt.Printf("ERROR: " + err.Error() + "\n\n")
+	}
 }
