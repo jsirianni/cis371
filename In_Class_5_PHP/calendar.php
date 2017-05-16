29c4f2dd816303b50c1ef907147b3cad01fd969a
jsirianni <jsirianni@ws1.sir.local>
2017-05-16 19:10:28 -0400

    change css dir

--- In_Class_5_PHP/calendar.php
+++ In_Class_5_PHP/calendar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 	<meta http-equiv="Content-type" content="text/html; charset=windows-1252"/>
-	<link rel="stylesheet" href="styles/default.css" type="text/css"/>
+	<link rel="stylesheet" href="default.css" type="text/css"/>
 	<title>Month Year</title>
 </head>
 <body>
@@ -53,7 +53,15 @@
           <th>Friday</th>
           <th>Saturday</th>
         </tr>
-        <tr>
+
+				<?php
+
+
+				?>
+
+
+
+				<tr>
 					PHP gen row 1
         </tr>
         <tr>
