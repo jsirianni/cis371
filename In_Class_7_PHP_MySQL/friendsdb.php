f7388416396d4a5cb266d23fbb8abe786289ddf5
jsirianni <jsirianni@ws1.sir.local>
2017-05-23 18:34:40 -0400

    begin formatting html

--- In_Class_7_PHP_MySQL/friendsdb.php
+++ In_Class_7_PHP_MySQL/friendsdb.php
@@ -68,7 +68,7 @@ function readTable() {
   $result = mysqli_query($sqlconn,$sql);
 
   // Display all DB content
-  <h3>Current Database Entries</h3>
+  echo "<h3>Current Database Entries</h3>";
   echo "<table><tbody>";
   echo "<tr><th>Id </th><th>First Name</th><th>Last Name</th><th>Phone Number</th><th>Age</th></tr>";
   while ($row = mysqli_fetch_assoc($result)) {
