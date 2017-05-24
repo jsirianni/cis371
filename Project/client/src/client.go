8439ffe09056a9f291136a31fe0d437ea28a0a9c
jsirianni <jsirianni@ws1.sir.local>
2017-05-23 21:01:46 -0400

    messign with loops

--- Project/client/src/client.go
+++ Project/client/src/client.go
@@ -1,23 +1,26 @@
 package main
+import (
+    "net"
+    "fmt"
+    "bufio"
+    "os"
+    "time"
+)
 
-import "net"
-import "fmt"
-import "bufio"
-import "os"
-
+// Connect to server and send report
 func main() {
 
-  // connect to this socket
-  conn, _ := net.Dial("tcp", "jsirianni.duckdns.org:8080")
+  // Build report
+  reader := bufio.NewReader(os.Stdin)
+  fmt.Print("Text to send: ")
+  text, _ := reader.ReadString('\n')
+
   for {
-    // read in input from stdin
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Text to send: ")
-    text, _ := reader.ReadString('\n')
-    // send to socket
-    fmt.Fprintf(conn, text + "\n")
-    // listen for reply
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Message from server: "+message)
+    connection, _ := net.Dial("tcp", "localhost:8090")
+    time.Sleep(1 * time.Second)
+    // send report and close connection
+    fmt.Fprintf(connection, text + "\n")
+
   }
+//connection.Close()
 }
--- Project/server/src/WebServer.go
+++ Project/server/src/WebServer.go
@@ -1,29 +1,12 @@
 package main
-
 import "net"
-import "fmt"
-import "bufio"
-import "strings" // only needed below for sample processing
 
+// Main routing handles connections on all interfaces
 func main() {
+  socket, _ := net.Listen("tcp", ":8090")
 
-  fmt.Println("Launching server...")
-
-  // listen on all interfaces
-  ln, _ := net.Listen("tcp", ":8081")
-
-  // accept connection on port
-  conn, _ := ln.Accept()
-
-  // run loop forever (or until ctrl-c)
   for {
-    // will listen for message to process ending in newline (\n)
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    // output message received
-    fmt.Print("Message Received:", string(message))
-    // sample process for string received
-    newmessage := strings.ToUpper(message)
-    // send new string back to client
-    conn.Write([]byte(newmessage + "\n"))
+    connection, _ := socket.Accept()
+    go handleClient(connection)
   }
 }
