e56e511dcbf5095d3fa87af154b041aa58f34a8e
jsirianni <jsirianni@ws1.sir.local>
2017-05-23 22:00:42 -0400

    testing server performance

--- Project/client/src/client.go
+++ Project/client/src/client.go
@@ -4,23 +4,35 @@ import (
     "fmt"
     "bufio"
     "os"
-    "time"
+    "strconv"
+    "strings"
 )
+
+
+// Global var
+var num int
+
 
 // Connect to server and send report
 func main() {
-
   // Build report
   reader := bufio.NewReader(os.Stdin)
   fmt.Print("Text to send: ")
   text, _ := reader.ReadString('\n')
 
+  // Send report as many times as possible
   for {
-    connection, _ := net.Dial("tcp", "localhost:8090")
-    time.Sleep(1 * time.Second)
-    // send report and close connection
-    fmt.Fprintf(connection, text + "\n")
-
+    sendReport(text)
   }
-//connection.Close()
+}
+
+
+func sendReport(t string) {
+  // Create connection and increment num
+  connection, _ := net.Dial("tcp", "localhost:8090")
+  num = num + 1
+
+  // send report and close connection
+  connection.Write([]byte(strings.TrimSuffix(t, "\n") + strconv.Itoa(num) + "\n"))
+  connection.Close()
 }
