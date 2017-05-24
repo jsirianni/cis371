96c32b090bfed568ecea9f97155bcdaa74e1c49f
jsirianni <jsirianni@ws1.sir.local>
2017-05-24 18:46:48 -0400

    dumby client finished

--- Project/server/src/WebServer.go
+++ Project/server/src/WebServer.go
@@ -1,12 +1,24 @@
 package main
-import "net"
+import (
+    "fmt"
+    "bufio"
+    "net"
+)
+
 
 // Main routing handles connections on all interfaces
 func main() {
   socket, _ := net.Listen("tcp", ":8090")
-
   for {
     connection, _ := socket.Accept()
     go handleClient(connection)
   }
+}
+
+
+// Go routine to read client reqport
+func handleClient(c net.Conn) {
+  message, _ := bufio.NewReader(c).ReadString('\n')
+  fmt.Print(string(message))
+  c.Close()
 }
