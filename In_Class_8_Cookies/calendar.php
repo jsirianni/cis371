d813c76c97544c3b34fd754a38fe27d260fe95b6
jsirianni <jsirianni@ws1.sir.local>
2017-05-26 19:50:40 -0400

    code cleanup

--- In_Class_8_Cookies/calendar.php
+++ In_Class_8_Cookies/calendar.php
@@ -28,7 +28,7 @@
 $cookie_name = "color";
 if ($_GET['backgroundColor_form'] == "blue" || $_GET['backgroundColor_form'] == "red" ) {
 	$cookie_value = $_GET['backgroundColor_form'];
-	setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/cis371/In_Class_8_Cookies/);
+	setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/cis371/In_Class_8_Cookies/");
 }
 echo "<p>browser cookie = $cookie_value</p>";
 ?>
