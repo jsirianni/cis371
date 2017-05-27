3a7fd2621e51be781d1cb274440b204cf77bb061
jsirianni <jsirianni@ws1.sir.local>
2017-05-27 13:13:44 -0400

    dev

--- Project/http_server/scripts/query.php
+++ Project/http_server/scripts/query.php
@@ -16,7 +16,7 @@ function setEnv() {
 //
 function readLast20($numOfReports) {
   setEnv();
-  $sqlconn =  mysqli_connect($dbhost, "reportuser", $dbuserpass, "report");
+  $sqlconn =  mysqli_connect($dbhost, "reportuser", "password", $dbname);
 
   if ($numOfReports != "") {
     $sql = "SELECT * FROM reports ORDER BY id DESC LIMIT $numOfReports";
