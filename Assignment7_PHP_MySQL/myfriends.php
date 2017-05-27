ab3ccbd587f76a1149c0625bd4cb1b4f7833cefe
jsirianni <jsirianni@ws1.sir.local>
2017-05-26 21:36:28 -0400

    add my friends page

--- Assignment7_PHP_MySQL/myfriends.php
+++ Assignment7_PHP_MySQL/myfriends.php
@@ -1,6 +1,15 @@
+<html>
+<link rel="stylesheet" href="default.css" type="text/css"/>
+<body>
+
 <?php
-// Import php functions
 include 'friendsdb.php';
+
+//
 // Print the table contents to the web page
+//
 readTable();
 ?>
+
+</body>
+</html>
