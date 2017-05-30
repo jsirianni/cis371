17591bfa872f2c0afda1413bac3d928bee3bdfd0
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-05-30 10:57:33 -0400

    dev

---
+++ midterm/mile-reimbursement.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+// Open files for reading
+$rRates = fopen("./reimbursement_rates.txt", "r");
+$iData = fopen("./invoice_data1.txt", "r");
+
+?>
+
+
+<!--Build the html content -->
+<html>
+<body>
+  <table><tbody>
+    <tr>
+      <th>Origin</th>
+      <th>Destination</th>
+      <th>Miles</th>
+      <th>Minutes</th>
+      <th>Avg. Speed</th>
+      <th>Rate</th>
+      <th>Total</th>
+    </tr>
+  </tbody></table>
+</body>
+</html>
