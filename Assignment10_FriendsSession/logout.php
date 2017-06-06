01b3a4e250fe5db194a01ff016c66ed55af7584b
jsirianni <jsirianni@ws1.sir.local>
2017-06-06 19:08:45 -0400

    dev

---
+++ Assignment10_FriendsSession/logout.php
@@ -0,0 +1,22 @@
+<?php
+include 'scripts/functions.php';
+
+$user = $_POST[''];
+$pass = $_POST[''];
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <link rel="stylesheet" href="styles/default.css" type="text/css"/>
+  <head>
+    <title>Login</title>
+    <h3>Friends Database</h3>
+  </head>
+  <body>
+    <div>
+      <p>You have been logged out</p>
+      <a href="login.php">Login</a>
+
+    </div>
+  </body>
+</html>
