e28fb24db43894ba1b170cec2440974fdafd3044
jsirianni <jsirianni@ws1.sir.local>
2017-06-06 19:21:26 -0400

    dev

--- Assignment10_FriendsSession/scripts/functions.php
+++ Assignment10_FriendsSession/scripts/functions.php
@@ -72,7 +72,7 @@ function accountLookup($username, $password) {
   $sqlconn = mysqli_connect("localhost", "root", "password", "cis371");
   $sql = "SELECT * FROM cis371.accounts WHERE username='$username' AND password='$password' LIMIT 1";
 
-  return mysqli_query($sqlconn,$sql) ? false : true;
+  return false;
   }
 }
 
