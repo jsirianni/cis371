64052e1d8adf40cd720b5c1e243dcd9da5793984
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-08 11:18:44 -0400

    dev

--- Assignment10_FriendsSession/home.php
+++ Assignment10_FriendsSession/home.php
@@ -2,8 +2,7 @@
 <?php
   if ( $_COOKIE['auth'] != "yes") {
     // Redirect user
-    //header("Location: home.php");
-    echo "<p>Not auth</p>";
+    header("Location: home.php");
   }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
