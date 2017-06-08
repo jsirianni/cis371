b33d75ddee7fb9b9085d74d7ad7c82e24b6a9037
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-08 11:12:25 -0400

    dev

--- Assignment10_FriendsSession/home.php
+++ Assignment10_FriendsSession/home.php
@@ -1,8 +1,9 @@
 
 <?php
-  if ($auth_status != 'yes'){
+  if ($auth_status !== "yes"){
     // Redirect user
-    header("Location: home.php");
+    //header("Location: home.php");
+    echo "<p>Not auth</p>";
   }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
