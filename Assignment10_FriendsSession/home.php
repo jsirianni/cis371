ac0ac5657f21472f95f2b015bf75cc8b42d0ee65
jsirianni <jsirianni@ws1.sir.local>
2017-06-07 23:27:10 -0400

    dev

--- Assignment10_FriendsSession/home.php
+++ Assignment10_FriendsSession/home.php
@@ -1,4 +1,7 @@
-<?php //if ($validLogin == 0) { header("Location: login.php");} ?>
+<?php
+  if ($validLogin === 0) {
+    header("Location: login.php");}
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
