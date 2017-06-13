9d16aa5f58e21ba525b0f75b4d7bae76f007fb7b
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-13 09:45:03 -0400

    dev

--- Project/http_server/quick-stats.php
+++ Project/http_server/quick-stats.php
@@ -17,7 +17,7 @@
           <a href="manual-report.php">Manual Report</a>
         </nav>
     </div>
-    <div class="body">
+    <div>
       <h4>Quick Stats</h4>
       <p>Displaying most recent reports, feel free to override</p>
       <form action="quick-stats.php">
