780dbf8a0b1d49475d2d20a863ac02b02e2ce73d
jsirianni <jsirianni@ws1.sir.local>
2017-06-14 18:52:37 -0400

    dev

--- Project/http_server/scripts/validate.js
+++ Project/http_server/scripts/validate.js
@@ -1,9 +1,13 @@
 function validateForm() {
     var x = document.forms["form"]["hostname"].value;
     var y = document.forms["form"]["status"].value;
-    if (x == "" || y =="") {
+    if (x == "" || y == "") {
         alert("Hostname and status are required");
         return false;
+    }
+    else if (y != "ok" || y != "bad") {
+      alert("Status be either 'ok' or 'bad'")
+      return false;
     }
     else {
       alert("The report has been submitted")
