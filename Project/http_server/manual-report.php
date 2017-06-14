df9c94bd07a59efa059cc67abd448d5ea2af7aca
jsirianni <jsirianni@ws1.sir.local>
2017-06-14 18:31:30 -0400

    dev

--- Project/http_server/manual-report.php
+++ Project/http_server/manual-report.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+<script src="scripts/validate.js"></script>
 <head>
     <link rel="stylesheet" href="styles/default.css" type="text/css"/>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
@@ -26,11 +27,13 @@
           A status other than "ok" is considered to be "bad" and will be caught by any
           filters that look for servers in a bad state.
         </p>
-        <form class="report" action="manual-report.php">
+
+        <form class="report" action="manual-report.php" onsubmit="return validateForm()>
           Hostname: <br><input type="text" name="hostname"><br>
           Status: <br><input type="text" name="status"><br>
           <input type="submit" value="Submit">
         </form>
+
         <?php
           include 'scripts/query.php';
           // Validate hostname
