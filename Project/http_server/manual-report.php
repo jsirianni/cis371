b59ada03a476fa51d70c98271acd92a815531b0a
jsirianni <jsirianni@ws1.sir.local>
2017-06-14 18:34:11 -0400

    dev

--- Project/http_server/manual-report.php
+++ Project/http_server/manual-report.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
+<script src="scripts/validate.js"></script>
 <head>
     <link rel="stylesheet" href="styles/default.css" type="text/css"/>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
