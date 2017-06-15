881eedd05144ee0cd9656c5ad688d3a6c17f3c19
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-15 11:41:04 -0400

    dev-final

--- Final/calendar.js
+++ Final/calendar.js
@@ -35,8 +35,8 @@ var update = function (date) {
 
     // Click handlers
     document.getElementById("rightArrow").addEventListener('click', function (e) {
+      var newDate = new Date(nextYear, nextMonth);
       alert("Right Arrow click");
-      var newDate = new Data(nextYear, nextMonth);
     })
     document.getElementById("leftArrow").addEventListener('click', function (e) {
       alert("Left Arrow click");
