bad4731bd9ea6dde17ed067785093d497c874e8e
jsirianni <jsirianni@ws1.sir.local>
2017-06-14 21:29:39 -0400

    dev

---
+++ Project/http_server/scripts/checksql.php
@@ -0,0 +1,31 @@
+<?php
+
+//
+// Set global values
+//
+function setGlobal() {
+  date_default_timezone_set("America/Detroit");
+  $GLOBALS['dbhost'] = 'localhost';
+  $GLOBALS['dbname'] = 'report';
+  $GLOBALS['ddbuser'] = 'reportuser';
+  $GLOBALS['dbuserpass'] = 'password';
+}
+
+function check(sql) {
+
+}
+
+// Begin script
+setGlobal();
+
+if ($_SERVER['REQUEST_METHOD']=="POST") {
+  echo "<p>Working</p>";
+}
+else {
+  echo "<p>Not Working</p>";
+}
+
+
+
+
+ ?>
