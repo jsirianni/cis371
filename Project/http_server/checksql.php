01b9ad8a5cfbf85054a719ca71432807acfaf844
jsirianni <jsirianni@ws1.sir.local>
2017-06-14 21:10:48 -0400

    dev

--- Project/http_server/checksql.php
+++ Project/http_server/checksql.php
@@ -1,7 +1,16 @@
 <?php
-include 'query.php';
-
+//
+// Set global values
+//
+function setGlobal() {
+  date_default_timezone_set("America/Detroit");
+  $GLOBALS['dbhost'] = 'localhost';
+  $GLOBALS['dbname'] = 'report';
+  $GLOBALS['ddbuser'] = 'reportuser';
+  $GLOBALS['dbuserpass'] = 'password';
+}
 setGlobal();
+
 $sql = $_POST['sql'];
 $sqlconn =  mysqli_connect($GLOBALS['dbhost'], $GLOBALS['ddbuser'], $GLOBALS['dbuserpass'], $GLOBALS['dbname']);
 if (!mysqli_query($sqlconn,$sql)) {
