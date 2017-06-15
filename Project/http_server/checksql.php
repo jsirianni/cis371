a2da2553bbd9fc527b6d46f68a3ba5dc833e60e4
jsirianni <jsirianni@ws1.sir.local>
2017-06-14 21:09:28 -0400

    dev

--- Project/http_server/checksql.php
+++ Project/http_server/checksql.php
@@ -1,20 +1,16 @@
 <?php
 include 'query.php';
-//
-// Check if valid sql. Returns true if valid
-//
-function checkSql() {
-  setGlobal();
-  $sql = $_POST['sql'];
-  $sqlconn =  mysqli_connect($GLOBALS['dbhost'], $GLOBALS['ddbuser'], $GLOBALS['dbuserpass'], $GLOBALS['dbname']);
-  if (!mysqli_query($sqlconn,$sql)) {
-    $sqlconn->close();
-    return false;
-  }
-  else {
-    $sqlconn->close();
-    return false;
-  }
+
+setGlobal();
+$sql = $_POST['sql'];
+$sqlconn =  mysqli_connect($GLOBALS['dbhost'], $GLOBALS['ddbuser'], $GLOBALS['dbuserpass'], $GLOBALS['dbname']);
+if (!mysqli_query($sqlconn,$sql)) {
+  $sqlconn->close();
+  return false;
+}
+else {
+  $sqlconn->close();
+  return false;
 }
 ?>
 <p>Validate your SQL</p>
