2c7f5bf66bd6e909c596b0d898827605a1c64421
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-15 12:19:27 -0400

    dev-final

--- Final/calendar.js
+++ Final/calendar.js
@@ -73,8 +73,8 @@ window.onload = function () {
   })
   document.getElementById("formsubmit").addEventListener('click', function (e) {
     // Pull data from form
-    var formYear =  document.getElementById("form_year").innerHTML;
-    var formMonth = document.getElementById("form_month").innerHTML;
+    var formYear =  1;//document.getElementById("form_year").innerHTML;
+    var formMonth = 2011;//document.getElementById("form_month").innerHTML;
     var newDate = new Date(formYear, formMonth);
     update(newDate);
 
