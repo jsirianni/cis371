7e8e1987a68331fc19ae6640da92a92b78a3c11d
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-15 11:48:06 -0400

    dev-final

--- Final/calendar.js
+++ Final/calendar.js
@@ -8,7 +8,7 @@
 var update = function (date) {
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var daysInCurrentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-    var monthWord = months[date.getMonth()];
+    var monthWord = months[date.getMonth()] + " " + date.getFullYear();
 
     // Set the header
     var header = document.getElementById("header");
