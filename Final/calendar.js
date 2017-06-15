6bb9d6634a173da3d77902e7974174c451bd3e35
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-15 10:50:26 -0400

    dev-final

--- Final/calendar.js
+++ Final/calendar.js
@@ -12,7 +12,7 @@ var update = function (date) {
     // *** Remember to use getElementsByTagName when possible.  It avoids a lot of the hassle of looking through each child. ***
     var dayCells = document.getElementsByTagName("td")
     var day = 1;
-    for (var i=0; i<elements.length; i++) {
+    for (var i=0; i<dayCells.length; i++) {
       dayCells[i].appendChild("day");
 
       var += 1;
