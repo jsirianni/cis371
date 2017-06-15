7b96d23863a69f8850470b37e0d99316638c261d
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-15 12:09:09 -0400

    Dev (#71)
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * begin final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final

--- Final/calendar.js
+++ Final/calendar.js
@@ -5,9 +5,25 @@
  *
  * @param date a JavaScript Date object set to a day in the month to be displayed.
  */
+
+
+// Global Vars
+var nextMonth;
+var nextYear;
+var prevMonth;
+var prevYear;
+
+
 var update = function (date) {
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var daysInCurrentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+
+
+    // Set the header month and year
+    var header = document.getElementById("header");
+    header.innerHTML = months[date.getMonth()] + " " + date.getFullYear();
+
 
     // Get all elements inside Calander Table, then get all data cells
     var calTable = document.getElementById("calendarTable");
@@ -23,15 +39,38 @@ var update = function (date) {
       day += 1;
     }
 
-    // Calculate the previous and next month
-    // (You may use this for adding links to the left arrow)
-    var nextMonth = date.getMonth()  + 1;
-    var nextYear = date.getFullYear();
+
+    // Calculate next month
+    nextMonth = date.getMonth()  + 1;
+    nextYear = date.getFullYear();
+
     if (nextMonth >= 12) {
         // Remember:  Months are numbered beginning with 0.
         nextMonth = 0;
         nextYear++;
     }
 
+    // Calculate previous month
+    prevMonth = date.getMonth() - 1;
+    prevYear = date.getFullYear();
+    if (prevMonth == -1) {
+      // Set month to december and decrease year by one
+      prevMonth = 11;
+      prevYear = prevYear - 1;
+    }
+
+
+}
+
+window.onload = function () {
+  // Click handlers
+  document.getElementById("rightArrow").addEventListener('click', function (e) {
+    var newDate = new Date(nextYear, nextMonth);
+    update(newDate);
+  })
+  document.getElementById("leftArrow").addEventListener('click', function (e) {
+    var newDate = new Date(prevYear, prevMonth);
+    update(newDate);
+  })
 
 }
