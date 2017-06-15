97aca239af682a2828ef6691937c48ae874170a8
Joseph Sirianni <joe.sirianni@bluemedora.com>
2017-06-15 11:24:03 -0400

    Dev (#70)
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * dev
    
    * begin final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final
    
    * dev-final

---
+++ Final/calendar.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ * Updates calendar.html to display the desired month.
+ *
+ * @param date a JavaScript Date object set to a day in the month to be displayed.
+ */
+var update = function (date) {
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var daysInCurrentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+    // Get all elements inside Calander Table, then get all data cells
+    var calTable = document.getElementById("calendarTable");
+    var dayCells = calTable.getElementsByTagName("td");
+
+    // Iterate through each cell and assign a day
+    // Stop when all days in month are filled
+    var day = 1;
+    for (var i=0; i<dayCells.length; i++) {
+      if (day <= daysInCurrentMonth) {
+        dayCells[i].innerHTML = day;
+      }
+      day += 1;
+    }
+
+    // Calculate the previous and next month
+    // (You may use this for adding links to the left arrow)
+    var nextMonth = date.getMonth()  + 1;
+    var nextYear = date.getFullYear();
+    if (nextMonth >= 12) {
+        // Remember:  Months are numbered beginning with 0.
+        nextMonth = 0;
+        nextYear++;
+    }
+
+
+}
